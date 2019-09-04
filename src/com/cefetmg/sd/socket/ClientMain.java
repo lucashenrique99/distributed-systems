4ac99260108453bd13d66e791e32eb79d156d9ae
Lucas <lucashenriques.dev@gmail.com>
2019-09-04 15:53:44 -0300

    Final implementation

--- src/com/cefetmg/sd/socket/ClientMain.java
+++ src/com/cefetmg/sd/socket/ClientMain.java
@@ -17,7 +17,6 @@ public class ClientMain {
             ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
             List<String> list = (List<String>) entrada.readObject();
             System.out.println(list.toString());
-//            entrada.close();
 
             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -28,11 +27,31 @@ public class ClientMain {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(cliente.getOutputStream());
             objectOutputStream.flush();
             objectOutputStream.writeObject(option);
-//            objectOutputStream.close();
 
 
             System.out.println("Recebendo musica");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(cliente.getInputStream());
+            AudioInputStream stream = AudioSystem.getAudioInputStream(bufferedInputStream);
+            AudioFormat format = stream.getFormat();
 
+            SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, stream
+                    .getFormat(), ((int) stream.getFrameLength() * format.getFrameSize()));
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(stream.getFormat());
+            line.start();
+
+            int numRead;
+            byte[] buf = new byte[line.getBufferSize()];
+            while ((numRead = stream.read(buf, 0, buf.length)) >= 0) {
+                int offset = 0;
+                while (offset < numRead) {
+                    offset += line.write(buf, offset, numRead - offset);
+                }
+            }
+            line.drain();
+            line.stop();
+
+            System.out.println("Cliente: fim musica");
 
         }
         catch(Exception e) {
--- src/com/cefetmg/sd/socket/Main.java
+++ src/com/cefetmg/sd/socket/Main.java
@@ -1,9 +1,7 @@
 package com.cefetmg.sd.socket;
 
 import javax.sound.sampled.*;
-import java.io.File;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
@@ -14,12 +12,10 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            // Instancia o ServerSocket ouvindo a porta 12345
+
             ServerSocket servidor = new ServerSocket(12345);
             System.out.println("Servidor ouvindo a porta 12345");
             while (true) {
-                // o método accept() bloqueia a execução até que
-                // o servidor receba um pedido de conexão
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
                 ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
@@ -27,49 +23,31 @@ public class Main {
                 saida.writeObject(
                         Arrays.asList("Musica 1", "Musica 2", "Musica 3")
                 );
-//                saida.close();
 
 
                 ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
                 String selected = (String) entrada.readObject();
-                System.out.println("Selected music: " + selected);
+                System.out.println("Musica selecionada: " + selected);
+                cliente.getOutputStream().flush();
 
-                AudioInputStream stream = AudioSystem
-                        .getAudioInputStream(
-                                new File("C:\\Users\\lucas\\Downloads\\coldplay_hymn_for_the_weekend_alan_walker_remix_music_video_64kbps.wav")
-                        );
+                File soundFile = new File("C:\\Users\\lucas\\Downloads\\coldplay_hymn_for_the_weekend_alan_walker_remix_music_video_64kbps.wav");
 
-                AudioFormat format = stream.getFormat();
-                if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
-                    format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, format
-                            .getSampleRate(), format.getSampleSizeInBits() * 2, format
-                            .getChannels(), format.getFrameSize() * 2, format.getFrameRate(),
-                            true); // big endian
-                    stream = AudioSystem.getAudioInputStream(format, stream);
+                System.out.println("Musica: " + soundFile);
+
+                FileInputStream in = new FileInputStream(soundFile);
+                OutputStream out = cliente.getOutputStream();
+
+                byte buffer[] = new byte[2048];
+                int count;
+                int sendCount = 0;
+                while ((count = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                    System.out.println("Send count: " + sendCount);
+                    sendCount++;
                 }
 
-                SourceDataLine.Info info = new DataLine.Info(SourceDataLine.class, stream
-                        .getFormat(), ((int) stream.getFrameLength() * format.getFrameSize()));
-                SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
-                line.open(stream.getFormat());
-                line.start();
 
-                int numRead = 0;
-                byte[] buf = new byte[line.getBufferSize()];
-                while ((numRead = stream.read(buf, 0, buf.length)) >= 0) {
-//                    int offset = 0;
-//                    while (offset < numRead) {
-//                        offset += line.write(buf, offset, numRead - offset);
-//                    }
-                    saida.flush();
-                    saida.writeObject(
-                           buf
-                    );
-
-                    Thread.sleep(800);
-                }
-                line.drain();
-                line.stop();
+                System.out.println("Servidor: finalizada transferencia");
             }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
